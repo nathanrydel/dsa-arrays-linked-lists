d570e3a55cf35ed84bc66f5e50e8c03e0a985ef0
Nathan Rydel <nathanrydel@gmail.com>
2024-02-27 10:58:45 -0700

    removed unneeded JS files

--- linked-lists/average/average.js
+++
@@ -1,8 +0,0 @@
-/** return average (mean) of list values.
- *
- * Returns 0 for empty list.
- **/
-function average(lst) {
-    return 42;
-}
-export { average };
--- linked-lists/rev-in-place/revInPlace.js
+++
@@ -1,4 +0,0 @@
-/** reverseInPlace() reverse list in place.*/
-function reverseInPlace(lst) {
-}
-export { reverseInPlace };
