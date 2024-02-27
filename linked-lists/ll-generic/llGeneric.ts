82197d02a893fe47d1d13d722569050e2681b928
Nathan Rydel <nathanrydel@gmail.com>
2024-02-27 10:58:32 -0700

    Updates to Linked List as a Generic

--- linked-lists/ll-generic/llGeneric.ts
+++ linked-lists/ll-generic/llGeneric.ts
@@ -33,16 +33,37 @@ class LL<T> {
     this.head = null;
     this.tail = null;
     this.length = 0;
+
+    for (const val of vals) this.push(val);
+  }
+
+  /** _getNodeAt(idx): retrieve node at idx.
+   *
+   * Returns null if not found.
+   **/
+
+  _getNodeAt(idx: number): LLNode<T> | null {
+    let curr: LLNode<T> | null = this.head;
+    let count = 0;
+
+    while (curr !== null && count !== idx) {
+      count++;
+      curr = curr.next;
+    }
+
+    return curr;
   }
 
   /** push(val): add new value to end of list. */
 
   push(val: T): void {
+    return this.insertAt(this.length, val);
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val: T): void {
+    return this.insertAt(0, val);
   }
 
   /** pop(): return & remove last item.
@@ -51,7 +72,7 @@ class LL<T> {
    **/
 
   pop(): T {
-    return "todo" as T;
+    return this.removeAt(this.length - 1);
   }
 
   /** shift(): return & remove first item.
@@ -60,7 +81,7 @@ class LL<T> {
    **/
 
   shift(): T {
-    return "todo" as T;
+    return this.removeAt(0);
   }
 
   /** getAt(idx): get val at idx.
@@ -69,7 +90,11 @@ class LL<T> {
    **/
 
   getAt(idx: number): T {
-    return "todo" as T;
+    if (idx >= this.length || idx < 0) {
+      throw new IndexError();
+    }
+
+    return this._getNodeAt(idx)!.val;
   }
 
   /** setAt(idx, val): set val at idx to val.
@@ -78,6 +103,12 @@ class LL<T> {
    **/
 
   setAt(idx: number, val: T): void {
+    if (idx >= this.length || idx < 0) {
+      throw new IndexError();
+    }
+
+    let curr = this._getNodeAt(idx);
+    curr!.val = val;
   }
 
   /** insertAt(idx, val): add node w/val before idx.
@@ -86,6 +117,35 @@ class LL<T> {
    **/
 
   insertAt(idx: number, val: T): void {
+    if (idx > this.length || idx < 0) {
+      throw new IndexError();
+    }
+
+    const newNode = new LLNode(val);
+
+    // unshift
+    if (idx === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+      if (this.length === 0) this.tail = newNode;
+    }
+
+    // push
+    else if (idx === this.length) {
+      const prev = this._getNodeAt(idx - 1);
+      newNode.next = prev!.next;
+      prev!.next = newNode;
+      this.tail = newNode;
+    }
+
+    // general case
+    else {
+      const prev = this._getNodeAt(idx - 1);
+      newNode.next = prev!.next;
+      prev!.next = newNode;
+    }
+
+    this.length++;
   }
 
   /** removeAt(idx): return & remove item at idx,
@@ -94,14 +154,43 @@ class LL<T> {
    **/
 
   removeAt(idx: number): T {
-    return "todo" as T;
-  }
+    if (idx >= this.length || idx < 0) {
+      throw new IndexError();
+    }
 
+    // shift
+    if (idx === 0) {
+      const curr = this.head!;
+      this.head = curr.next;
+      if (this.head === null) this.tail = null;
+      this.length--;
+      return curr.val;
+    }
+
+    // general case
+    else {
+      const prev = this._getNodeAt(idx - 1);
+      const curr = prev!.next!;
+      prev!.next = curr.next;
+
+      if (idx === this.length - 1) this.tail = prev;
+
+      this.length--;
+      return curr.val;
+    }
+  }
 
   /** toArray (useful for tests!) */
 
   toArray(): T[] {
-    return [];
+    const out: T[] = [];
+    let curr = this.head;
+
+    while (curr) {
+      out.push(curr.val);
+      curr = curr.next;
+    }
+    return out;
   }
 }
 
@@ -109,13 +198,13 @@ class LL<T> {
 // syntax to use the generic LL<T> class, here are pre-made versions
 // for strings and numbers:
 
-class LLStr extends LL<string> {}
+class LLStr extends LL<string> { }
 
-class LLNum extends LL<number> {}
+class LLNum extends LL<number> { }
 
-class LLNodeStr extends LLNode<string> {}
+class LLNodeStr extends LLNode<string> { }
 
-class LLNodeNum extends LLNode<number> {}
+class LLNodeNum extends LLNode<number> { }
 
 export {
   IndexError,
